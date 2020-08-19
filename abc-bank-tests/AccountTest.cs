b2a7a1b39d395cbd92f174574231c6fee851db64
Ryan Bergeman <code@ryanbergeman.com>
2020-08-19 10:28:01 -0500

    Add tests around depositing and withdrawing
    This tries to ensure the feature "A customer can deposit / withdraw
    funds from an account."
    
    * Ensure zero and negative amounts cannot be either deposited nor
      withdrawn
    
    * Change `ArgumentException` to the more specific
      `ArgumentOutOfRangeException`

---
+++ abc-bank-tests/AccountTest.cs
@@ -0,0 +1,71 @@
+﻿using abc_bank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace abc_bank_tests
+{
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void Deposit_AddsTransactionIncreasingAccountBalance()
+        {
+            Account savings = new Account(Account.CHECKING);
+
+            savings.Deposit(2000.0);
+
+            Assert.AreEqual(2000.0, savings.sumTransactions());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Deposit_RejectsNegativeAmount()
+        {
+            Account checking = new Account(Account.CHECKING);
+            checking.Deposit(1000.0);
+
+            checking.Deposit(-500.0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Deposit_RejectsAmountOfZero()
+        {
+            Account savings = new Account(Account.SAVINGS);
+            savings.Deposit(500.0);
+
+            savings.Deposit(0);
+        }
+
+        [TestMethod]
+        public void Withdraw_AddsTransactionDecreasingAccountBalance()
+        {
+            Account savings = new Account(Account.SAVINGS);
+            savings.Deposit(500.0);
+
+            savings.Withdraw(250.0);
+
+            Assert.AreEqual(250.0, savings.sumTransactions());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Withdraw_RejectsNegativeAmount()
+        {
+            Account checking = new Account(Account.CHECKING);
+            checking.Deposit(250.0);
+
+            checking.Withdraw(-100.0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Withdraw_RejectsAmountOfZero()
+        {
+            Account checking = new Account(Account.CHECKING);
+            checking.Deposit(500.0);
+
+            checking.Withdraw(0.0);
+        }
+    }
+}
--- abc-bank/Account.cs
+++ abc-bank/Account.cs
@@ -22,22 +22,24 @@ namespace abc_bank
             this.transactions = new List<Transaction>();
         }
 
-        public void Deposit(double amount) 
+        public void Deposit(double amount)
         {
-            if (amount <= 0) {
-                throw new ArgumentException("amount must be greater than zero");
-            } else {
-                transactions.Add(new Transaction(amount));
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "must be greater than zero");
             }
+
+            transactions.Add(new Transaction(amount));
         }
 
-        public void Withdraw(double amount) 
+        public void Withdraw(double amount)
         {
-            if (amount <= 0) {
-                throw new ArgumentException("amount must be greater than zero");
-            } else {
-                transactions.Add(new Transaction(-amount));
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "must be greater than zero");
             }
+
+            transactions.Add(new Transaction(-amount));
         }
 
         public double InterestEarned() 
